feebd9f3d9efac0497468bfd4c31cc01a6b9a4d3
Mantu <mantu.kumar@itechnolabs.tech>
2023-06-16 19:15:59 +0530

    Add notification API with changed design

--- src/screens/Setting/Components/Notification.js
+++ src/screens/Setting/Components/Notification.js
@@ -20,46 +20,66 @@ import {
   scanner,
   toggleSecurity,
   trackCamera,
+  vector,
+  vectorOff,
 } from '@/assets';
 import { deviceDropDownArray } from '@/constants/flatListData';
 import { moderateScale, verticalScale } from 'react-native-size-matters';
+import { upadteApi } from '@/actions/SettingAction';
+import { useDispatch, useSelector } from 'react-redux';
+import { getSetting } from '@/selectors/SettingSelector';
 
 export function Notification() {
-  const renderFlatList = data => (
-    <FlatList
-      data={data}
-      ItemSeparatorComponent={() => (
-        <View style={{ marginVertical: verticalScale(3) }} />
-      )}
-      renderItem={({ item, index }) => (
-        <View style={styles.flexRow}>
-          <Text style={styles.notificationName}>{item.name}</Text>
-          <Image source={toggleSecurity} style={styles.toggleSecurity} />
-        </View>
-      )}
-      keyExtractor={item => item.key}
-    />
-  );
+  const dispatch = useDispatch();
+  const getSettingData = useSelector(getSetting);
 
-  const appNotificationArray = useMemo(
-    () => [
-      {
-        key: '1',
-        name: 'News and Updates',
-        value: 'notification_status',
-      },
-      {
-        key: '2',
-        name: 'News and Updates',
-        value: 'email_notification_status',
-      },
-      {
-        key: '3',
-        name: 'News and Updates',
-        value: 'push_notification_status',
-      },
-    ],
-    []
+  const [appNotificationArray, setAppNotificationArray] = useState([
+    {
+      key: '1',
+      name: 'Allow notifications',
+      value: 'notification_status',
+      isSelected: getSettingData?.getSetting?.notification_status ?? false,
+    },
+    {
+      key: '2',
+      name: 'Email Notifications',
+      value: 'email_notification_status',
+      isSelected:
+        getSettingData?.getSetting?.email_notification_status ?? false,
+    },
+    {
+      key: '3',
+      name: 'Push notifications',
+      value: 'push_notification_status',
+      isSelected: getSettingData?.getSetting?.push_notification_status ?? false,
+    },
+  ]);
+
+  const handleToggle = index => {
+    setAppNotificationArray(prevState => {
+      const newArray = [...prevState];
+      newArray[index].isSelected = !newArray[index].isSelected;
+
+      const data = {
+        [newArray[index].value]: newArray[index].isSelected,
+        app_name: 'pos',
+      };
+      dispatch(upadteApi(data));
+
+      return newArray;
+    });
+  };
+
+  const renderItem = ({ item, index }) => (
+    <View style={styles.flexRow}>
+      <Text style={styles.notificationName}>{item.name}</Text>
+      <TouchableOpacity onPress={() => handleToggle(index)}>
+        <Image
+          source={item.isSelected ? vector : vectorOff}
+          style={styles.toggleSecurity}
+        />
+      </TouchableOpacity>
+    </View>
   );
 
   return (
@@ -71,25 +91,17 @@ export function Notification() {
       </View>
       <Spacer space={SH(20)} />
       <View style={styles.notificationMainCon}>
-        <ScrollView>
-          <View style={{ paddingHorizontal: moderateScale(10) }}>
-            <Text style={styles.appNotification}>App Notification</Text>
-            <View style={styles.horizontalRow} />
-            {renderFlatList(appNotificationArray)}
-          </View>
-          <Spacer space={SH(30)} />
-          <View style={{ paddingHorizontal: moderateScale(10) }}>
-            <Text style={styles.appNotification}>SMS Notification</Text>
-            <View style={styles.horizontalRow} />
-            {renderFlatList(appNotificationArray)}
-          </View>
-          <Spacer space={SH(30)} />
-          <View style={{ paddingHorizontal: moderateScale(10) }}>
-            <Text style={styles.appNotification}>Email Notification</Text>
-            <View style={styles.horizontalRow} />
-            {renderFlatList(appNotificationArray)}
-          </View>
-        </ScrollView>
+        <View style={{ paddingHorizontal: moderateScale(10) }}>
+          <View style={styles.horizontalRow} />
+          <FlatList
+            data={appNotificationArray}
+            ItemSeparatorComponent={() => (
+              <View style={{ marginVertical: verticalScale(3) }} />
+            )}
+            renderItem={renderItem}
+            keyExtractor={item => item.key}
+          />
+        </View>
       </View>
     </View>
   );
