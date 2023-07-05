bdceea6f013f780d2785b52578477a7ffd894cb6
Mantu <mantu.kumar@itechnolabs.tech>
2023-07-05 15:17:26 +0530

    Add react-native-big-calendar as a CustomComponent in project
    - Remove react-native-big-calendar  from package.json (Now we are importing it from components)

---
+++ src/components/CustomCalendar/components/HourGuideCell.tsx
@@ -0,0 +1,53 @@
+import dayjs from 'dayjs';
+import * as React from 'react';
+import { TouchableWithoutFeedback, View } from 'react-native';
+
+import { u } from '../commonStyles';
+import { CalendarCellStyle } from '../interfaces';
+import { useTheme } from '../theme/ThemeContext';
+
+interface HourGuideCellProps {
+  cellHeight: number;
+  onPress: (d: dayjs.Dayjs) => void;
+  date: dayjs.Dayjs;
+  hour: number;
+  index: number;
+  calendarCellStyle?: CalendarCellStyle;
+}
+
+const _HourGuideCell = ({
+  cellHeight,
+  onPress,
+  date,
+  hour,
+  index,
+  calendarCellStyle,
+}: HourGuideCellProps) => {
+  const theme = useTheme();
+
+  const getCalendarCellStyle = React.useMemo(
+    () =>
+      typeof calendarCellStyle === 'function'
+        ? calendarCellStyle
+        : () => calendarCellStyle,
+    [calendarCellStyle]
+  );
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => onPress(date.hour(hour).minute(0))}
+    >
+      <View
+        style={[
+          u['border-l'],
+          u['border-b'],
+          { borderColor: theme.palette.gray['200'] },
+          { height: cellHeight },
+          { ...getCalendarCellStyle(date.toDate(), index) },
+        ]}
+      />
+    </TouchableWithoutFeedback>
+  );
+};
+
+export const HourGuideCell = React.memo(_HourGuideCell);
