c481a0d82fdc521d7a5127fde57d9c2e6586c8dc
Mantu <mantu.kumar@itechnolabs.tech>
2023-09-07 17:08:57 +0530

    Calendar: New design changes in calendar Reschedule appointment

---
+++ src/components/ServiceProviderItem.js
@@ -0,0 +1,31 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { COLORS } from '@/theme';
+import { ms } from 'react-native-size-matters';
+
+export const ServiceProviderItem = ({ item, onPress, borderColor, containerStyle, imageStyle }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={[styles.imageSelectedBorder, containerStyle, { borderColor }]}
+  >
+    <Image
+      source={{ uri: item?.user?.user_profiles?.profile_photo }}
+      style={[styles.imageStyl, imageStyle]}
+    />
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  imageSelectedBorder: {
+    borderWidth: 5,
+    marginHorizontal: ms(5),
+    borderRadius: 100,
+    borderColor: COLORS.primary,
+  },
+  imageStyl: {
+    width: ms(45),
+    height: ms(45),
+    resizeMode: 'contain',
+    borderRadius: ms(22.5),
+  },
+});
