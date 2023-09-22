c902d48f502881e577f87bcf422b6ef95a3d314f
jobr.tech <Driver@jobr.tech>
2023-09-22 16:54:33 +0530

    wallet issue solved

--- src/screens/Wallet2/Components/WeeklyTransaction.js
+++ src/screens/Wallet2/Components/WeeklyTransaction.js
@@ -94,6 +94,17 @@ export function WeeklyTransaction({ backHandler, orderClickHandler, comeFrom }) 
   const orderTypeSelection = (value) => setOrderTypeSelect(value);
   const [orderTypeSelect, setOrderTypeSelect] = useState('none');
 
+  const orderTypeArray = [
+    {
+      label: 'Product',
+      value: 'product',
+    },
+    {
+      label: 'Service',
+      value: 'service',
+    },
+  ];
+
   const dummyArea = [
     {
       label: 'Shimla',
@@ -167,9 +178,10 @@ export function WeeklyTransaction({ backHandler, orderClickHandler, comeFrom }) 
       limit: paginationModalValue,
       sellerId: sellerID,
       calendarDate: formatedDate,
+      orderType: orderTypeSelect,
     };
     dispatch(getTotakTraDetail(data));
-  }, [selectId, transaction, page, paginationModalValue, formatedDate]);
+  }, [selectId, transaction, page, paginationModalValue, formatedDate, orderTypeSelect]);
 
   const onChangeDate = (selectedDate) => {
     setDefaultDate(selectedDate);
@@ -225,6 +237,9 @@ export function WeeklyTransaction({ backHandler, orderClickHandler, comeFrom }) 
         break;
       case 8:
         return 'Rejected';
+        break;
+      case 9:
+        return 'Returned';
         break;
     }
   };
@@ -367,7 +382,11 @@ export function WeeklyTransaction({ backHandler, orderClickHandler, comeFrom }) 
             <TableDropdown placeholder="Status" selected={statusSelection} data={dummyArea} />
           </View>
           <>
-            <TableDropdown placeholder="Order type" selected={orderTypeSelection} data={months} />
+            <TableDropdown
+              placeholder="Order type"
+              selected={orderTypeSelection}
+              data={orderTypeArray}
+            />
           </>
         </View>
       </View>
