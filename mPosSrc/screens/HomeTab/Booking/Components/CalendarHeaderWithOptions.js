864f1504b0aab082c200b787bdb9abf87883b318
Mansi <delivery@jobr.tech>
2023-11-10 17:37:20 +0530

    Worked on Calendar module

--- mPosSrc/screens/HomeTab/Booking/Components/CalendarHeaderWithOptions.js
+++ mPosSrc/screens/HomeTab/Booking/Components/CalendarHeaderWithOptions.js
@@ -13,12 +13,6 @@ const CalendarHeaderWithOptions = ({
   prevMonth,
   getFormattedHeaderDate,
   nextMonth,
-  day,
-  dayHandler,
-  week,
-  weekHandler,
-  month,
-  monthHandler,
   time,
   setTime,
   timeValue,
@@ -62,7 +56,7 @@ const CalendarHeaderWithOptions = ({
                   setOpen={setTime}
                   setValue={setTimeValue}
                   setItems={setTimeItem}
-                  placeholder="Weekly"
+                  placeholder="Week"
                   placeholderStyle={{
                     color: '#A7A7A7',
                     fontFamily: Fonts.Regular,
