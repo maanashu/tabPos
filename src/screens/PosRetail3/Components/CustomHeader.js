531d59fc240098da3ff998b5d3802aa3f668bf46
iTechno <itechno@iTechnos-Mac-mini.local>
2023-11-28 15:13:15 +0530

    header in users ui auth changes

--- src/screens/PosRetail3/Components/CustomHeader.js
+++ src/screens/PosRetail3/Components/CustomHeader.js
@@ -10,7 +10,7 @@ import { getUser } from '@/selectors/UserSelectors';
 
 moment.suppressDeprecationWarnings = true;
 
-export function CustomHeader({ crossHandler, iconShow }) {
+export function CustomHeader({ crossHandler, iconShow, showUserName = true }) {
   const getUserData = useSelector(getUser);
   const getPosUser = getUserData?.posLoginData;
   return (
@@ -26,23 +26,27 @@ export function CustomHeader({ crossHandler, iconShow }) {
         </View>
 
         {/* <View style={styles._border} /> */}
-        <View style={styles.displayRow}>
-          <Image
-            source={
-              getPosUser?.user_profiles?.profile_photo
-                ? { uri: getPosUser?.user_profiles?.profile_photo }
-                : userImage
-            }
-            style={styles.iconStyle}
-          />
-          <Text style={styles.cashLabelBold}>
-            {getPosUser?.user_profiles?.firstname ?? 'username'}(
-            {getPosUser?.user_roles?.length > 0
-              ? getPosUser?.user_roles?.map((item, index) => item.role?.name)
-              : 'admin'}
-            )
-          </Text>
-        </View>
+        {showUserName ? (
+          <View style={styles.displayRow}>
+            <Image
+              source={
+                getPosUser?.user_profiles?.profile_photo
+                  ? { uri: getPosUser?.user_profiles?.profile_photo }
+                  : userImage
+              }
+              style={styles.iconStyle}
+            />
+            <Text style={styles.cashLabelBold}>
+              {getPosUser?.user_profiles?.firstname ?? 'username'}(
+              {getPosUser?.user_roles?.length > 0
+                ? getPosUser?.user_roles?.map((item, index) => item.role?.name)
+                : 'admin'}
+              )
+            </Text>
+          </View>
+        ) : (
+          <View style={{ flex: 0.5 }} />
+        )}
         {/* <View style={styles._border} /> */}
         <Text style={styles.cashLabelBold}>POS No. {getUserData?.posLoginData?.pos_number}</Text>
         <View style={styles.walkinCon}>
