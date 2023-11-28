0c2b71fbbb8a84319be63da11aeab8d87bdff5a9
iTechno <itechno@iTechnos-Mac-mini.local>
2023-11-28 19:04:38 +0530

    custom header changes

--- src/screens/PosRetail3/Components/CustomHeader.js
+++ src/screens/PosRetail3/Components/CustomHeader.js
@@ -7,6 +7,7 @@ import { Image } from 'react-native';
 import moment from 'moment';
 import { useSelector } from 'react-redux';
 import { getUser } from '@/selectors/UserSelectors';
+import { ms } from 'react-native-size-matters';
 
 moment.suppressDeprecationWarnings = true;
 
@@ -15,53 +16,62 @@ export function CustomHeader({ crossHandler, iconShow, showUserName = true }) {
   const getPosUser = getUserData?.posLoginData;
   return (
     <View style={styles.searchScreenHeader}>
-      <View style={styles.displayflex}>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <Text style={styles.cashLabelBold}>{moment().format('hh:mm a')}</Text>
-          <View style={styles._border} />
-          {/* <Text style={styles.cashLabelBold}>{moment().format('ddd DD MMM, YYYY')}</Text> */}
-          <Text style={styles.cashLabelBold}>{moment().format('LLL')}</Text>
+      <View style={[styles.displayflex, { marginHorizontal: ms(24) }]}>
+        <Image
+          source={logo_icon}
+          style={{ height: ms(42), width: ms(30), borderRadius: ms(60), marginRight: ms(32) }}
+          resizeMode="contain"
+        />
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', borderWidth: 1 }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Text style={styles.cashLabelBold}>{moment().format('hh:mm a')}</Text>
+            <View style={styles._border} />
+            {/* <Text style={styles.cashLabelBold}>{moment().format('ddd DD MMM, YYYY')}</Text> */}
+            <Text style={styles.cashLabelBold}>{moment().format('LLL')}</Text>
+
+            {/* <View style={styles._border} /> */}
+          </View>
 
           {/* <View style={styles._border} /> */}
-        </View>
-
-        {/* <View style={styles._border} /> */}
-        {showUserName ? (
-          <View style={styles.displayRow}>
-            <Image
-              source={
-                getPosUser?.user_profiles?.profile_photo
-                  ? { uri: getPosUser?.user_profiles?.profile_photo }
-                  : userImage
-              }
-              style={styles.iconStyle}
-            />
+          {showUserName ? (
+            <View style={styles.displayRow}>
+              <Image
+                source={
+                  getPosUser?.user_profiles?.profile_photo
+                    ? { uri: getPosUser?.user_profiles?.profile_photo }
+                    : userImage
+                }
+                style={styles.iconStyle}
+              />
+              <Text style={styles.cashLabelBold}>
+                {getPosUser?.user_profiles?.firstname ?? 'username'}(
+                {getPosUser?.user_roles?.length > 0
+                  ? getPosUser?.user_roles?.map((item, index) => item.role?.name)
+                  : 'admin'}
+                )
+              </Text>
+            </View>
+          ) : (
+            <View style={{ flex: 0.5 }} />
+          )}
+          {/* <View style={styles._border} /> */}
+          {getUserData?.posLoginData?.pos_number && (
             <Text style={styles.cashLabelBold}>
-              {getPosUser?.user_profiles?.firstname ?? 'username'}(
-              {getPosUser?.user_roles?.length > 0
-                ? getPosUser?.user_roles?.map((item, index) => item.role?.name)
-                : 'admin'}
-              )
+              POS No. {getUserData?.posLoginData?.pos_number}
             </Text>
+          )}
+          <View style={styles.walkinCon}>
+            <Text style={styles.cashLabelBold}>Walk-In</Text>
           </View>
-        ) : (
-          <View style={{ flex: 0.5 }} />
-        )}
-        {/* <View style={styles._border} /> */}
-        {getUserData?.posLoginData?.pos_number && (
-          <Text style={styles.cashLabelBold}>POS No. {getUserData?.posLoginData?.pos_number}</Text>
-        )}
-        <View style={styles.walkinCon}>
-          <Text style={styles.cashLabelBold}>Walk-In</Text>
-        </View>
 
-        {iconShow ? (
-          <TouchableOpacity onPress={crossHandler}>
-            <Image source={crossButton} style={styles.crossBg} />
-          </TouchableOpacity>
-        ) : (
-          <View></View>
-        )}
+          {iconShow ? (
+            <TouchableOpacity onPress={crossHandler}>
+              <Image source={crossButton} style={styles.crossBg} />
+            </TouchableOpacity>
+          ) : (
+            <View></View>
+          )}
+        </View>
       </View>
     </View>
   );
