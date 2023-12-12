aca4ed3591e66fd892e2ef5353e876109e43ad11
Manpreet <delivery@jobr.tech>
2023-12-12 12:35:35 +0530

    implemented chat in support menu

--- mPosSrc/screens/MoreTab/Help/SupportChat/SupportChat.js
+++ mPosSrc/screens/MoreTab/Help/SupportChat/SupportChat.js
@@ -1,79 +1,41 @@
 import { COLORS } from '@/theme';
-import React, { useState, useCallback, useEffect } from 'react';
-import { TouchableOpacity, View, Image, Text, Keyboard, ActivityIndicator } from 'react-native';
-import { GiftedChat, Send, InputToolbar, MessageImage, Bubble } from 'react-native-gifted-chat';
-import Modal from 'react-native-modal';
-import {
-  messageSend,
-  attachPic,
-  addAttachment,
-  closeX,
-  attachIcon,
-  AttachIcon,
-  pin,
-} from '@/assets';
+import React, { useState, useEffect } from 'react';
+import { View, Image, ActivityIndicator, StatusBar } from 'react-native';
+import { GiftedChat, Send, InputToolbar } from 'react-native-gifted-chat';
+import { messageSend } from '@/assets';
 import { ms } from 'react-native-size-matters';
-import { Fonts, crossButton } from '@/assets';
 import io from 'socket.io-client';
-import ProfileImage from '@/components/ProfileImage';
-import { useFocusEffect } from '@react-navigation/native';
 import { getUser } from '@/selectors/UserSelectors';
-import { useDispatch, useSelector } from 'react-redux';
-import { getMessages, sendChat } from '@/actions/ChatAction';
-import { Loader } from '@/components/Loader';
-import { TYPES } from '@/Types/Types';
-import { isLoadingSelector } from '@/selectors/StatusSelectors';
-import { getMessagesData } from '@/selectors/ChatSelector';
+import { useSelector } from 'react-redux';
 import { Header, ScreenWrapper } from '@mPOS/components';
 import styles from './SupportChat.styles';
-// const socket = io('https://apichat.jobr.com:8007/');
 
 export const SupportChat = (props) => {
-  const dispatch = useDispatch();
-  const [isSocketConnected, setIsSocketConnected] = useState(false);
   const [messagesData, setMessages] = useState([]);
   const [messageHeadId, setMessageHeadId] = useState('');
   const [recipientId, setRecipientId] = useState();
 
-  const [bottomOffset, setbottomOffset] = useState(0);
-  const [showView, setShowView] = useState('');
-  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
-  const [attachModal, setAttachModal] = useState(false);
   const [isLoading, setisLoading] = useState(false);
-  const [isLoadingMsg, setisLoadingMsg] = useState(false);
   const user = useSelector(getUser);
-  const chatData = useSelector(getMessagesData);
-  const USER_DATA = user?.posLoginData?.user_profiles;
   const ticketId = props?.route?.params?.id;
 
   const socket = io(`https://apichat.jobr.com:8007?userId=${user?.posLoginData?.uuid}`, {
     path: '/api/v1/connect',
   });
-  useFocusEffect(
-    React.useCallback(() => {
-      setisLoading(true);
-      socket.on('connect', () => {
-        setIsSocketConnected(true);
-        console.log('Connected');
-        getMessages();
-      });
-      return () => {
-        socket.on('disconnect', () => {
-          setIsSocketConnected(false);
-        });
-        socket.disconnect();
-      };
-    }, [])
-  );
 
-  const getMessages = () => {
-    socket.emit('get_messages', {
-      id: ticketId.toString(),
-      idtype: 'ticket_id',
+  useEffect(() => {
+    setisLoading(true);
+    socket.on('connect', () => {
+      console.log('Connected');
+      getMessages();
+    });
+
+    socket.on('send_message', (msg) => {
+      console.log('message sent console', msg);
+      setMessageHeadId(msg?.data?.chathead_id);
     });
 
     socket.on('get_messages', (message) => {
-      console.log('messages', message?.data?.data?.[0]);
       setRecipientId(message?.data?.data?.[0]?.recipient_id);
       setMessageHeadId(message?.data?.data?.[0]?.chathead_id);
       const arr = message?.data?.data.map((item) => {
@@ -89,6 +51,40 @@ export const SupportChat = (props) => {
       setMessages(arr);
       setisLoading(false);
     });
+
+    socket.on('disconnect', () => {
+      console.log('disconnected');
+    });
+
+    return () => {
+      socket.off('get_messages');
+      socket.off('send_message');
+      socket.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    socket.on(`JobrUser_${messageHeadId}_room`, (data) => {
+      const newMsg = {
+        _id: data?.data?._id,
+        text: data?.data?.content,
+        createdAt: data?.data?.createdAt,
+        user: {
+          _id: data?.data?.sender_id,
+        },
+      };
+      setMessages((prev) => [...prev, newMsg]);
+    });
+    return () => {
+      socket.off(`JobrUser_${messageHeadId}_room`);
+    };
+  }, [messageHeadId]);
+
+  const getMessages = () => {
+    socket.emit('get_messages', {
+      id: ticketId.toString(),
+      idtype: 'ticket_id',
+    });
   };
 
   const onSend = (messages = []) => {
@@ -96,27 +92,11 @@ export const SupportChat = (props) => {
       recipient_id: recipientId,
       media_type: 'text',
       content: messages[0]?.text,
-      chatHeadType: 'directchat',
+      chatHeadType: 'inquiry',
       sender_id: user?.posLoginData?.uuid,
+      ticket_id: ticketId.toString(),
     };
-    console.log('payload ' + JSON.stringify(params));
     socket.emit('send_message', params);
-
-    socket.on('send_message', (msg) => {
-      console.log('message sent console', msg);
-      setMessageHeadId(msg?.data?.chathead_id);
-      socket.on(`JobrUser_${messageHeadId || msg?.data?.chathead_id}_room`, (data) => {
-        console.log('room resp', data);
-        const newMsg = {
-          _id: data?.data?._id,
-          text: data?.data?.content,
-          user: {
-            _id: data?.data?.sender_id,
-          },
-        };
-        setMessages([...messagesData, newMsg]);
-      });
-    });
   };
 
   const renderSend = (props) => {
@@ -129,13 +109,15 @@ export const SupportChat = (props) => {
 
   return (
     <ScreenWrapper>
+      <StatusBar barStyle="dark-content" backgroundColor={COLORS.white} />
+
       <Header backRequired title={'Support'} style={{ backgroundColor: COLORS.white }} />
 
       <View style={styles.loaderView}>
         {isLoading && <ActivityIndicator color={COLORS.primary} size={'large'} />}
       </View>
       <GiftedChat
-        messages={messagesData}
+        messages={[...messagesData]?.reverse()}
         showAvatarForEveryMessage={false}
         renderAvatar={false}
         onSend={(messages) => onSend(messages)}
