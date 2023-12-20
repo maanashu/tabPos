f3b169604aa12502cd60a96fc829eb2ce1cb6bbf
Mantu <mantu.kumar@itechnolabs.tech>
2023-12-20 12:39:18 +0530

    Fix invalid host crash on physical devices

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,9 +1,16 @@
 import Reactotron from 'reactotron-react-native';
 import { AsyncStorage } from '@react-native-async-storage/async-storage';
 import { reactotronRedux } from 'reactotron-redux';
+import { NativeModules } from 'react-native';
+
+let scriptHostname;
+if (__DEV__) {
+  const scriptURL = NativeModules.SourceCode.scriptURL;
+  scriptHostname = scriptURL.split('://')[1].split(':')[0];
+}
 
 const reactotron = Reactotron.setAsyncStorageHandler(AsyncStorage)
-  .configure() // controls connection & communication settings
+  .configure({ host: scriptHostname }) // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
   .use(reactotronRedux())
   .connect(); // let's connect!
